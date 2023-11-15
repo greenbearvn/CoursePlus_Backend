58e2e275af9817fa520c00d90088cefb0b9813f3
greenbearvn <hungvu2352002@gmail.com>
2023-11-15 20:28:39 +0700

    fix login

--- src/app/controllers/frontend/AccountController.js
+++ src/app/controllers/frontend/AccountController.js
@@ -13,12 +13,9 @@ class AccountController {
       const nguoidung = req.body;
       const account = nguoidung.user;
 
-      const saltRounds = 10;
-      const salt = await bcrypt.genSalt(saltRounds);
+      const hashedPassword = await bcrypt.hash(account.MatKhau, 10);
 
-      const hashPass = await bcrypt.hash(account.MatKhau, salt);
-
-      account.MatKhau = hashPass;
+      account.MatKhau = hashedPassword;
       account.Quyen = 0;
 
       const query = `INSERT INTO nguoidung (TenNguoiDung, Email, MatKhau, Quyen)
@@ -48,6 +45,7 @@ class AccountController {
       });
     }
   };
+
   login = async (req, res) => {
     try {
       const nguoidung = req.body;
@@ -55,99 +53,37 @@ class AccountController {
       const sql = `SELECT * FROM nguoidung where Email = '${account.Email}'`;
       const results = await this.db.query(sql, []);
       const resUser = results[0];
-      const user = req.session.user || []; // Initialize as an array if it doesn't exist
-  
-      console.log(account.MatKhau, resUser.MatKhau);
-  
+      const user = req.session.user || [];
+
       if (resUser) {
         const isMatch = await bcrypt.compare(account.MatKhau, resUser.MatKhau);
-  
-        if (user) {
+
+        if (isMatch) {
           const userData = {
             MaNguoiDung: resUser.MaNguoiDung,
             TenNguoiDung: resUser.TenNguoiDung,
             Email: resUser.Email,
             Quyen: resUser.Quyen,
           };
-  
+
           user.push(userData);
-  
-          res.status(200).json({
+
+          return res.status(200).json({
             data: user,
           });
-          
         }
-        
       }
-      res.status(404).json({
+      return res.status(404).json({
         error: "User not found",
       });
     } catch (error) {
       console.error(error);
-      res.status(500).json({
+      return res.status(500).json({
         ok: false,
         error: "Something went wrong!",
       });
     }
   };
-
-  // login = async (req, res) => {
-  //   try {
-  //     const nguoidung = req.body;
-  //     const account = nguoidung.user;
-  //     const sql = `SELECT * FROM nguoidung where Email = '${account.Email}'`;
-  //     const results = await this.db.query(sql, []);
-  //     const resUser = results[0];
-  //     const user = req.session.user;
-
-  //     console.log(account.MatKhau, resUser.MatKhau);
-
-  //     if (resUser) {
-  //       const isMatch = await bcrypt.compare(account.MatKhau, resUser.MatKhau);
-
-  //       if (!user) {
-  //         const userData = {
-  //           MaNguoiDung: resUser.MaNguoiDung,
-  //           TenNguoiDung: resUser.TenNguoiDung,
-  //           Email: resUser.Email,
-  //           Quyen: resUser.Quyen,
-  //         };
-
-  //         req.session.user.push(userData);
-
-  //         res.status(200).json({
-  //           data: user,
-  //         });
-  //         return; // Thêm lệnh return để kết thúc hàm sau khi gửi phản hồi thành công
-  //       }
-  //       // if (isMatch) {
-  //       //   if (!user) {
-  //       //     userData = {
-  //       //       MaNguoiDung: resUser.MaNguoiDung,
-  //       //       TenNguoiDung: resUser.TenNguoiDung,
-  //       //       Email: resUser.Email,
-  //       //       Quyen: resUser.Quyen,
-  //       //     };
-  //       //     user.push(userData);
-
-  //       //     res.status(200).json({
-  //       //       data: user,
-  //       //     });
-  //       //     return; // Thêm lệnh return để kết thúc hàm sau khi gửi phản hồi thành công
-  //       //   }
-  //       // }
-  //     }
-  //     res.status(404).json({
-  //       error: "User not found",
-  //     });
-  //   } catch (error) {
-  //     console.error(error);
-  //     res.status(500).json({
-  //       ok: false,
-  //       error: "Something went wrong!",
-  //     });
-  //   }
-  // };
 }
 
 module.exports = new AccountController();
