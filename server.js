60556e855ea0188109769c4c0aec0d258e2089c1
greenbearvn <hungvu2352002@gmail.com>
2023-11-20 23:44:32 +0700

    add demo chat using socket.io

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const http = require("http");
 const { Server } = require("socket.io");
-const session = require("express-session")
+const session = require("express-session");
 const app = express();
 
 const server = http.createServer(app);
@@ -12,7 +12,7 @@ const body_parser = require("body-parser");
 
 app.use("/uploads", express.static("uploads"));
 app.use(body_parser.urlencoded({ extended: false }));
-app.use(cors({origin:"*"}));
+app.use(cors({ origin: "*" }));
 app.use(body_parser.json());
 
 app.use(
@@ -26,6 +26,28 @@ app.use(
 
 const route = require("./src/app/routes/route");
 app.use("/api", route);
+
+// socket io
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
+
+io.on("connection", (socket) => {
+  socket.on("join_room", (data) => {
+    socket.join(data.room);
+    socket.broadcast.to(data.room).emit("user joined");
+  });
+
+  socket.on("send_message", (data) => {
+    io.to(data.room).emit("receive_message", data);
+  });
+
+  socket.on("disconnect", () => {});
+});
 
 server.listen(8000, () => {
   console.log("Server is running on port 8000");
